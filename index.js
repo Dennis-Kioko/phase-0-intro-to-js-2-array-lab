401d9bfcfa21ec2b4bc16e62d9f1e4bb5390612a
denniskioko <kiokodennis6990@gmail.com>
2023-12-14 17:16:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+let cats = ["Milo","Otis","Garfield"];
+console.log(cats);
+
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop(cats);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    let newCats = ["Milo","Otis","Garfield","Broom"];
+    return newCats;
+}
+
+function prependCat() {
+    let newCats = ["Arnold","Milo","Otis","Garfield"];
+    newCats.unshift();
+    return newCats;
+
+}
+
+
+function removeLastCat() {
+    let newCats = ["Milo"];
+    newCats.push("Otis");
+    return newCats;
+}
+
+
+function removeFirstCat()
+{
+    let newCats = ['Milo','Otis','Garfield'];
+    newCats.shift();
+    return newCats;
+}
